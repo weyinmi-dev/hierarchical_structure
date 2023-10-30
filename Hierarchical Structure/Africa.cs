6594c599bfb80f3de957317036347cc587ca0f9d
weyinmi-dev <weyinmi.e.madamedon@@gmail.com>
2023-10-30 13:27:38 +0100

    first commit

---
+++ Hierarchical Structure/Africa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hierarchical_Structure
+{
+    interface Africa : Continent
+    {
+
+    }
+}
---
+++ Hierarchical Structure/Bison.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hierarchical_Structure
+{
+    public class Bison : Hebivore, America
+    {
+
+    }
+}
